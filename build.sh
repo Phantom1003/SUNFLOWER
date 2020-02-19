55830bacb6918385c9998ed661330068c66af36b
Yan <892930505@qq.com>
2020-02-19 23:03:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,12 +7,11 @@ export RISCV=~/SUNFLOWER
 
 echo "Starting RISC-V Toolchain build process"
 
-build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 build_project riscv-gnu-toolchain --prefix=$RISCV
 CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
-cd SUNFLOWER
+cd sunflower-test
 echo -e "\nCompile project test files"
 make
 echo -e "\\nRISC-V Toolchain installation completed!"
